7d627497d0f74bc525ffac518df680b8b274a554
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2023-04-03 10:53:11 -0500

    commit tree 1ec0e7696b3ee848e68bbec3129ec1b53d3e1118

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,26 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    # byebug
+    if !params[:name]
+
+      students = Student.all
+      render json: students
+    else
+
+      # student_last = Student.find_by(last_name: params[:name].capitalize)
+      student_last = Student.where("last_name = ?", params[:name])
+
+
+      student_first = Student.find_by(first_name: params[:name].capitalize)
+
+      if student_first
+        render json: student_first
+      else
+        render json: student_last
+      end
+
+    end
   end
 
   def show
